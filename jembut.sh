eb8f83fb1381dca2b219ab21e15feeacda016b21
InYourLw7 <44433891+InYourLw7@users.noreply.github.com>
2018-10-24 12:49:58 +0000

    Create jembut.sh

---
+++ jembut.sh
@@ -0,0 +1,471 @@
+clear 
+#!/system/xbin/bash
+#ngapain lu bangsat
+#mau record?
+#apa faedah nya?#biar di bilang mastah?
+#kok gua ngukuk ya
+#lu ngerecord gak bakal ngubah lu jadi mastah goblok
+#klo gak suka gelud aja kyta!1!1
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#colors
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f slant --gay "Selamat Datang"
+echo  $cyan"  [✮]<><><><><><><><><><><><><><><><><><><><><>[✮]"
+echo  $cyan"  [✮]     $white  Author :./InY#urLW7""  $cyan              [✮]"
+echo  $cyan"  [✮]     $white  Team :2Easy4Hack""$cyan                   [✮]"
+echo  $cyan"  [✮]     $white  Contact :0895378276565""$cyan             [✮]"
+echo  $white"  [✮]     $cyan  Tolls versi:1""$white                      [✮]"
+echo  $white"  [✮]     $cyan  Thanks to all member 2E4H""$white          [✮] "
+echo  $white"  [✮]     $cyan  Gunakan Tools dengan bijak""$white         [✮] "
+echo  $white"  [✮]<><><><><><><><><><><><><><><><><><><><><>[✮]"
+echo  $cyan
+figlet "InY#urLW7  v1"
+echo " Hanya tols mewbie"|lolcat
+echo " jadi wajar lah kalo jelek :("|lolcat
+echo ""
+echo $cyan "[1].Install RED_HAWK"
+echo $cyan "[2].Install Websploit"
+echo $cyan "[3].Tembak Kouta xl"
+echo $cyan "[4].Install LITESCRIPT"
+echo $cyan "[5].Install LITESPAM"
+echo $cyan "[6].Install Santet online"
+echo $cyan "[7].Install katoolin"
+echo $cyan "[8].Install Findip"
+echo $cyan "[9].Install sqlmap"
+echo $cyan "[10].Install adminpenal"
+echo $cyan "[11].Install Tolls ICA"
+echo $cyan  "[12].Install hydra"
+echo $white "[13].Install hunner"
+echo $white "[14].Install Wpscan"
+echo $white "[15].Install Linux fedora"
+echo $white "[16].Install sudo (Root)"
+echo $white "[17].Install BinGoo"
+echo $white "[18].Install Bajingan v6"
+echo $white "[19].Install Cuup"
+echo $white "[20].Install The Fat Rat"
+echo $white "[21].Install MBF bua hek efbi"
+echo $white "[22].Install Lazymux"
+echo $white "[23].Install bot awto depes njenk :<"
+echo $red    "[00].$white keluar/crootz"
+read -p " Pilih Nomor>>> " pilihan
+
+if [ $pilihan = "00" ] || [ $pilihan = "00" ]
+then
+echo $cyan
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami dan semoga bermanfaat"
+fi
+
+if [ $pilihan = "1" ] || [ $pilihan = "01" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update&&apt upgrade
+apt install php
+apt install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd RED_HAWK"
+echo $cyan "chmod +x rhawk.php"
+echo $cyan "php rhawk.php"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "2" ] || [ $pilihan = "02" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && upgrade -y
+apt install python2
+pip2 install scapy
+pkg install git
+git clone https://github.com/The404Hacking/websploit.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd websploit"
+echo $cyan "chmod 777 *.py"
+echo $cyan "python2 websploit.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "3" ] || [ $pilihan = "03" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+pkg install python
+pkg install git
+git clone https://github.com/kumpul4semut/semut.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd semut"
+echo $cyan "python dor.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "4" ] || [ $pilihan = "04" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+pkg install python2 git
+git clone https://github.com/4L13199/LITESCRIPT
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd LITESCRIPT"
+echo $cyan "python2 LITESCRIPT.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "5" ] || [ $pilihan = "05" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update
+apt upgrade
+pkg install php
+pkg install git
+pkg install toilet
+git clone https://github.com/4L13199/LITESPAM
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd LITESPAM"
+echo $cyan "sh LITESPAM.sh"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "6" ] || [ $pilihan = "06" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade 
+apt install python2
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd santet-online"
+echo $cyan "python2 santet.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "7" ] || [ $pilihan = "07" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+pkg install git
+pkg install python2
+pkg install gnupg
+pkg install nano
+git clone https://github.com/LionSec/katoolin.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd katoolin"
+echo $cyan "python2 katoolin.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "8" ] || [ $pilihan = "08" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+pkg install git
+git clone https://github.com/kereh/Findip
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd Findip"
+echo $cyan "python2 findip.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "9" ] || [ $pilihan = "09" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+apt install python2
+git clone https://github.com/sqlmapproject/sqlmap.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd sqlmap"
+echo $cyan "python2 sqlmap.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "10" ] || [ $pilihan = "10" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+pkg install git
+git clone https://github.com/Techzindia/admin_penal/
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd admin_penal"
+echo $cyan "python2 main.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "11" ] || [ $pilihan = "11" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade -y
+apt install git
+git clone https://github.com/21D4N404/Defacer
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd Defacer"
+echo $cyan "sh ICA.sh"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "12" ] || [ $pilihan = "12" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt install -y wget
+apt install hydra
+wget http://download1887.mediafire.com/aw4lmg5yvhcg/5tvpocv5gijo0dc/darkc0de.lst 
+-O darkc0de.lst
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "hydra"
+echo $cyan "hydra -h"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "13" ] || [ $pilihan = "13" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+apt install python
+apt install git
+git clone https://github.com/b3-v3r/Hunner.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd Hunner"
+echo $cyan "chmod +x hunner.py"
+echo $cyan "python hunner.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "14" ] || [ $pilihan = "14" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+apt install git
+apt install ruby
+git clone https://github.com/wpscanteam/wpscan/
+cd wpscan
+chmod 777 wpscan.rb
+gem install bundle
+bundle install -j5
+ruby wpscan.rb
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd wpscan"
+echo $cyan "chmod 777 wpscan.rb"
+echo $cyan "gem install bundle"
+echo $cyan "bundle install -j5"
+echo $cyan "bundle install -j5"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "15" ] || [ $pilihan = "15" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt install git
+apt install wget
+git clone https://github.com/nmilosev/termux-fedora.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "ls"
+echo $cyan "cd termux-fedora"
+echo $cyan "chmod +x termux-fedora.sh"
+echo $cyan "./termux-fedora.sh f25_arm <-- contoh"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "16" ] || [ $pilihan = "16" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+apt install git
+git clone https://github.com/st42/termux-sudo
+cd termux-sudo
+apt install ncurses-utils
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "sudo"
+echo $cyan "sudo su"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "17" ] || [ $pilihan = "17" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update &&amp; upgrade -y
+apt install curl
+apt install grep
+apt install linx
+apt install git
+git clone https://github.com/Hood3dRob1n/BinGoo.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd BinGoo"
+echo $cyan "termux-fix-shebang bingoo"
+echo $cyan "chmod +x bingoo"
+echo $cyan "bash bingoo"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "18" ] || [ $pilihan = "18" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update apt upgrade
+apt install git
+apt install php 
+apt install figlet 
+apt install toilet 
+apt install python2
+pip2 install requests 
+pip2 install termcolor
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd BAJINGANv6"
+echo $cyan "sh BAJINGAN.sh"
+echo $cyan "user name :BAJINGAN"
+echo $cyan "password : Gans"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "19" ] || [ $pilihan = "19" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update && apt upgrade
+apt install python2 git
+git clone https://github.com/Mebus/cupp.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd cupp"
+echo $cyan "python2 cupp.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "20" ] || [ $pilihan = "20" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt-get update && apt-get upgrade
+apt-get install git
+git clone https://github.com/Screetsec/TheFatRat.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd TheFatRat"
+echo $cyan "chmod +x setup.sh"
+echo $cyan "./setup.sh"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "21" ] || [ $pilihan = "21" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+pkg update && upgrade
+pkg install python2 
+pkg install git
+pip2 install mechanize
+git clone https://github.com/pirmansx/mbf
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd MBF"
+echo $cyan "python2 MBF.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "22" ] || [ $pilihan = "22" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt update
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd Lazymux"
+echo $cyan "python2 Lazymux.py"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
+
+if [ $pilihan = "23" ] || [ $pilihan = "23" ]
+then
+cowsay -f tux "InY#urLW7"|lolcat
+echo $cyan "sabar tod proses penginstalan"
+apt-get update && apt-get upgrade
+apt-get install git
+apt-get install wget
+apt-get install perl
+apt-get install unzip
+git clone https://github.com/mrcakil/bot.git
+echo $cyan"<><><><><><><><><><><><><><><>"
+echo $cyan "udh selesai tod"
+echo $cyan "cara pakai"
+echo $cyan "cd bot"
+echo $cyan "unzip bot.zip"
+echo $cyan "cd xploit"
+echo $cyan "chmod 777 bot.pl"
+echo $cyan "Lokasi bot ? /bot/xploit/bot.pl"
+echo $white"<><><><><><><><><><><><><><><>"
+fi
